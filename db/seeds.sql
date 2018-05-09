2749ed96c879beb5ad6cb1e8a0239f4dae76ed6b
GTrapp16 <GTrapp16@gmail.com>
2018-05-08 20:54:59 -0700

    initial attempt

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken-Bacon", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Whata", false);
